9572eab38fb2dd5b04c7c5153272a7c09f6acac2
Brian Jenkins <studiojenkins@yahoo.com>
2021-03-15 13:03:07 -0400

    edit

--- server.js
+++ server.js
@@ -1,16 +1,13 @@
 const express = require('express');
 const path = require('path');
 const fs = require('fs');
-
 const app = express();
-app.use(express.urlencoded({ extended: true}));
+
+const apioutes = require('./routes/apiroutes')(app);
+const htmlroutes = require('./routes/htmlroutes')(app);app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 const port = process.env.port || 8080;
-
-const apioutes = require('./routes/apiroutes')(app);
-const htmlroutes = require('./routes/htmlroutes')(app);
-
 
 app.use(express.static('public'));
 
