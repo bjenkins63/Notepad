06875bb6367196dff3455f1bb3a817112b828471
Brian Jenkins <studiojenkins@yahoo.com>
2021-03-16 10:44:37 -0400

    final revise

--- routes/apiroutes.js
+++ routes/apiroutes.js
@@ -1,27 +1,53 @@
+// Dependencies
+// =============================================================
+// Express router functions
 const router = require("express").Router();
-const store = require("../db/store");
+// Allows the generation of unique ids
+const { v4: uuidv4 } = require('uuid');
+// Get access to the database file
+const dbData = require('../db/db');
 
-// GET "/api/notes" responds with all notes from the database
-router.get("/notes", function(req, res) {
-  store
-    .getNotes()
-    .then(notes => res.json(notes))
-    .catch(err => res.status(500).json(err));
+// API routes
+// =============================================================
+// Get the JSON file
+router.get("/notes", (req, res) => {
+    try {
+        res.json(dbData);
+        // res.json(Notes.readNotes());
+    } catch (err) {
+        res.status(500).end();
+    }
+});
+// Edit the JSON file with the new note
+router.post("/notes", async (req, res) => {
+    // Create the new note
+    let newNote = 
+    {
+        title: req.body.title,
+        text: req.body.text,
+        id: uuidv4(),
+    }
+    try {
+        dbData.push(newNote);
+        res.json(dbData);
+    } catch (err) {
+        res.status(500).end();
+    }
+});
+// Delete the note with the specified UUID
+router.delete("/notes/:id", (req, res) => {
+    // Find the index value of the object with matching ids
+    let noteIndex = dbData.findIndex( element => {
+        return element.id == req.params.id
+    });
+    try {
+        dbData.splice(noteIndex,1);
+        res.json(dbData);
+    } catch (err) {
+        res.status(500).end();
+    }
 });
 
-router.post("/notes", (req, res) => {
-  store
-    .addNote(req.body)
-    .then((note) => res.json(note))
-    .catch(err => res.status(500).json(err));
-});
-
-// DELETE "/api/notes" deletes the note with an id equal to req.params.id
-router.delete("/notes/:id", function(req, res) {
-  store
-    .removeNote(req.params.id)
-    .then(() => res.json({ ok: true }))
-    .catch(err => res.status(500).json(err));
-});
-
+// Exports
+// =============================================================
 module.exports = router;
